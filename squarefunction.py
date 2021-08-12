291b2c1f7cf0a3f9dfe35d2916c1d654c6249c8e
Objectivitix <79152594+Objectivitix@users.noreply.github.com>
2021-08-12 14:26:37 -0300

    More examples, some bug fixes, and add method support

--- squarefunction.py
+++ squarefunction.py
@@ -1,17 +1,45 @@
-from typing import Any, Callable, NoReturn
+from typing import Any, Callable, NoReturn, Optional, Union
+
+
+def tuplefy(obj: Any) -> tuple:
+    return obj if isinstance(obj, tuple) else (obj,)
+
+
+class SquareMethod:
+    """
+    Class implementing method support for SquareFunction.
+
+    To expand on this, class and static method equivalents can also be easily created.
+    """
+    def __init__(self, func: "SquareFunction", instance: Any) -> None:
+        """Construct a SquareMethod instance bounded to obj."""
+        self.__func__ = func
+        self.__self__ = instance
+
+    def __getitem__(self, poskwargs: Any) -> None:
+        """
+        Perform a method call with the bounded object prepended to inputted arguments.
+
+        Call the __getitem__ of the underlying SquareFunction.
+        """
+        self.__func__.__getitem__((self.__self__,) + tuplefy(poskwargs))
 
 
 class SquareFunction:
     """
     Class for functions who are invoked with square brackets instead of parentheses.
 
-    Complete support for argument defaults, pos-only args, pos-or-kw args, and kwargs.
+    Complete support for argument defaults, all types of arguments, and even instance methods!
     Normal slice objects whose 'start' attributes are anything other than strings are allowed.
     """
 
     def __init__(self, func: Callable[..., Any]) -> None:
         """Construct a SquareFunction instance."""
         self.func = func
+
+    @staticmethod
+    def _is_valid_kwarg(arg: Any) -> bool:
+        return isinstance(arg, slice) and isinstance(arg.start, str)
 
     @staticmethod
     def _verify(pkargs: tuple) -> None:
@@ -23,16 +51,13 @@ class SquareFunction:
         riter = reversed(pkargs)
         posarg_start = False
         try:
-            start_with_kwarg = isinstance(next(riter), slice)
+            start_with_kwarg = SquareFunction._is_valid_kwarg(next(riter))
         except StopIteration:
             return
 
         for arg in riter:
-            if isinstance(arg, slice):
-                if (
-                    not (start_with_kwarg and not posarg_start)
-                    and isinstance(arg.start, str)
-                ):
+            if SquareFunction._is_valid_kwarg(arg):
+                if not (start_with_kwarg and not posarg_start):
                     raise SyntaxError(
                         "positional arguments were found "
                         "after keyword arguments"
@@ -46,20 +71,16 @@ class SquareFunction:
         """Split a tuple of args and kwargs into a list of args and a dict of kwargs."""
         posargs, kwargs = [], {}
         for arg in pkargs:
-            if isinstance(arg, slice):
-                if isinstance(arg.start, str):
-                    kwargs[arg.start] = arg.stop
-                else:
-                    posargs.append(arg)
+            if SquareFunction._is_valid_kwarg(arg):
+                kwargs[arg.start] = arg.stop
             else:
                 posargs.append(arg)
 
         return posargs, kwargs
 
-    def __getitem__(self, args_and_kwargs: Any) -> None:
+    def __getitem__(self, poskwargs: Any) -> None:
         """Invoke the underlying function when subscription is performed."""
-        ak = args_and_kwargs
-        pkargs: tuple = ak if isinstance(ak, tuple) else (ak,)
+        pkargs = tuplefy(poskwargs)
 
         self._verify(pkargs)
         posargs, kwargs = self._split(pkargs)
@@ -76,11 +97,38 @@ class SquareFunction:
             "journey to enlightenment, where we use square brackets [] instead!"
         )
 
+    def __get__(
+            self, instance: Any, owner: Optional[type] = None
+    ) -> Union["SquareFunction", "SquareMethod"]:
+        """Clone of standard function descriptor behaviour."""
+        if instance is None:
+            return self
+        else:
+            return SquareMethod(self, instance)
+
+
+# ----- DRIVER CODE -----
+
 
 @SquareFunction
 def yeet(a=1, b=2, /, c=3, *, d=4, e=5):
-    print([0, 1, 2, 3, 4][a])
+    if isinstance(d, slice):
+        print([0, 1, 2, 3, 4][d], end="   ")
     print(a, b, c, d, e)
 
 
-yeet[1:4, (50,), "e":7]
+class Yeet:
+    @SquareFunction
+    def foo(self, x, y):
+        self.x, self.y = x, y
+
+
+yeet[10]                              # output: 10 2 3 4 5
+yeet[10, 20, "c":30, "d":40, "e":50]  # output: 10 20 30 40 50
+yeet["d": slice(1, 4)]                # output: [1, 2, 3]   1 2 3 slice(1, 4, None) 5
+# yeet[1:4, 3, "d":4, 2:5]            # error: posargs were found after kwargs
+
+yeet_obj = Yeet()
+yeet_obj.foo[5, "y":3]
+print(yeet_obj.x, yeet_obj.y)         # output: 5 3
+print(Yeet.foo)                       # output: <__main__.SquareFunction object at 0x000001ECC142A790>
